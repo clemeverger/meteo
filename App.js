f3e9f445c32dedfd07879856fc5d140647f801b5
Clément Verger <clemeverger@gmail.com>
2022-11-18 11:51:25 +0100

    Screen icons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 
 import * as React from 'react';
+import { Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -16,17 +17,33 @@ const App = () => {
       <NavigationContainer>
         <Tab.Navigator screenOptions={{
           headerShown: false,
-          tabBarIcon: () => { return null },
           tabBarActiveTintColor: theme.colors.base,
           tabBarInactiveTintColor: theme.colors.base,
           tabBarLabelStyle: { fontSize: 15 },
           tabBarStyle: { backgroundColor: theme.colors.secondary }
         }}>
-          <Tab.Screen name="Home" component={Stack} />
-          <Tab.Screen name="Credit" component={Credit} />
+          <Tab.Screen
+            name="Home"
+            component={Stack}
+            options={{ tabBarIcon: () => <Image style={styles.icon} source={require('./src/assets/home.png')}></Image> }}
+          />
+          <Tab.Screen
+            name="Credit"
+            component={Credit}
+            options={{ tabBarIcon: () => <Image style={styles.icon} source={require('./src/assets/credit.png')}></Image> }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </QueryClientProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain'
+  },
+});
+
 export default App;
